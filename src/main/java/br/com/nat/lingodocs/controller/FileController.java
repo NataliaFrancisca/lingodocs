16e59b03a316cc78c8c9d21fc3f3d4b104886a16
nataliafrancisca <nathifrancisca@gmail.com>
2025-11-21 19:05:10 -0300

    feat: add "OPTIONS" to requestMatchers

--- src/main/java/br/com/nat/lingodocs/controller/FileController.java
+++ src/main/java/br/com/nat/lingodocs/controller/FileController.java
@@ -1,6 +1,6 @@
 package br.com.nat.lingodocs.controller;
 
-import br.com.nat.lingodocs.dto.bucket.FileRequest;
+import br.com.nat.lingodocs.dto.auth.response.MessageResponse;
 import br.com.nat.lingodocs.dto.bucket.FileResponse;
 import br.com.nat.lingodocs.dto.bucket.FilesResponse;
 import br.com.nat.lingodocs.service.model.FileService;
@@ -24,10 +24,10 @@ public class FileController {
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file, Authentication auth){
+    public ResponseEntity<MessageResponse> upload(@RequestParam("file") MultipartFile file, Authentication auth){
         var username = getUsernameFromAuth(auth);
         this.service.upload(file, username);
-        return ResponseEntity.ok().body("Upload realizado com sucesso.");
+        return ResponseEntity.ok().body(new MessageResponse("response: ", "Upload realizado com sucesso."));
     }
 
     @GetMapping("/all")
@@ -38,9 +38,9 @@ public class FileController {
     }
 
     @GetMapping
-    public ResponseEntity<FileResponse> get(@RequestBody FileRequest fileRequest, Authentication auth){
+    public ResponseEntity<FileResponse> get(@RequestParam String fileName, Authentication auth){
         var username = getUsernameFromAuth(auth);
-        var response = this.service.get(fileRequest, username);
+        var response = this.service.get(fileName, username);
         return ResponseEntity.ok().body(response);
     }
 }
